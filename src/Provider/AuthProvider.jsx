9f11bffba38832647b8ee196c7cf6bac9aa21da2
unknown <arafat.innotimes.net@gmail.com>
2024-12-12 21:56:58 +0600

    Firebase connection

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,46 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {
+    onAuthStateChanged,
+    signOut,
+} from "firebase/auth";
+import auth from "../Firebase/firebase.config";
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+   
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+  
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        
+        return () => unSubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        logOut,
+    };
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
